88051453afbcdbda0c6de40db77a24671d6e1868
J-Spade <j_spade@verizon.net>
2021-05-01 14:59:00 -0400

    reformat with black. make adjustments recommended by pylint. test_spy.py: --post now implies --test

--- forum_spy.py
+++ forum_spy.py
@@ -1,37 +1,43 @@
-from bs4 import BeautifulSoup
-import discord
+"""
+Watches the starmen.net forum spy for new posts, and posts them into #forum-spy
+on the official discord server.
+"""
 import json
 import os
+import sys
 import time
 import urllib.request
+
+from bs4 import BeautifulSoup
+import discord
 
 
 # # # # #
 # Config variables
 # # # # #
 
-webhook_url = os.getenv('FORUM_SPY_DISCORD_WEBHOOK_URL')
+webhook_url = os.getenv("FORUM_SPY_DISCORD_WEBHOOK_URL")
 if not webhook_url:
-    print('ERROR: Environment variable FORUM_SPY_DISCORD_WEBHOOK_URL must be set!')
-    exit()
+    print("ERROR: Environment variable FORUM_SPY_DISCORD_WEBHOOK_URL must be set!")
+    sys.exit()
 
 DISCORD_WEBHOOK = discord.Webhook.from_url(webhook_url, adapter=discord.RequestsWebhookAdapter())
 DISCORD_NO_MENTIONS = discord.AllowedMentions(everyone=False, users=False, roles=False)
 
-FORUM_ROOT = 'https://forum.starmen.net'
-FORUM_SPY_AJAX = FORUM_ROOT + '/forum/spy.ajax'
+FORUM_ROOT = "https://forum.starmen.net"
+FORUM_SPY_AJAX = FORUM_ROOT + "/forum/spy.ajax"
 FORUM_SPY_REQUEST_HEADERS = {
-    'Accept': '*/*',
-    'Referer': 'https://forum.starmen.net/forum/spy',
-    'User-Agent': 'discord-forum-spy-bot',
+    "Accept": "*/*",
+    "Referer": "https://forum.starmen.net/forum/spy",
+    "User-Agent": "discord-forum-spy-bot",
 }
 FORUM_PREVIEW_LENGTH = 250
-FORUM_COLOR_EVEN = int(0x010b17)
+FORUM_COLOR_EVEN = int(0x010B17)
 FORUM_COLOR_ODD = int(0x001228)
 
 EXCLUDED_BOARDS = [
-    '/forum/Community/mafia',
-    '/forum/Community/mafiB',
+    "/forum/Community/mafia",
+    "/forum/Community/mafiB",
 ]
 
 # What to display when we can't fit a quote?
@@ -46,43 +52,43 @@ MIN_QUOTE_LENGTH = 5
 # Helper functions
 # # # # #
 
+
 def _get_username(user_profile):
-    '''
+    """
     Requests the user profile and parses the name out of it (used for members with avatars).
     If the request fails, just default to the URL string.
-    '''
+    """
     try:
         profile_request = urllib.request.Request(user_profile, headers=FORUM_SPY_REQUEST_HEADERS)
-        with urllib.request.urlopen(profile_request) as f:
-            data = f.read()
+        with urllib.request.urlopen(profile_request) as response:
+            data = response.read()
     except urllib.error.HTTPError as err:
-        print(f'While querying {user_profile}, {err.code}: {err.reason}')
-        return user_profile.split('/')[-1]
+        print(f"While querying {user_profile}, {err.code}: {err.reason}")
+        return user_profile.split("/")[-1]
 
-    soup = BeautifulSoup(data, 'html.parser')
-    member = soup.find('a', {'class': 'member'})
+    soup = BeautifulSoup(data, "html.parser")
+    member = soup.find("a", {"class": "member"})
     return member.string
 
+
 def _format_quotes(content, max_length, nesting):
-    # Handle blockquotes and spoiler blocks
+    """
+    Handle blockquotes and spoiler blocks. Recursively formats inner quote content the same way.
 
-    # Blockquotes: recursively formats inner quote content the same way
-    # We want to only show quotes if we have enough room after truncation
-    # (i.e. we don't want to have the preview be just quotes.)
-    # So we see how much space should be devoted to non-quote text and
-    # then divide up remaining space among quotes which will get shortened.
-    # (unless the remaining size per quote is very short or zero in which
-    #  case we'll just replace the quote with '[...]')
-
+    We want to only show quotes if we have enough room after truncation (i.e. we don't want to have
+    the preview be just quotes.) So we see how much space should be devoted to non-quote text and
+    then divide up remaining space among quotes which will get shortened. (unless the remaining
+    size per quote is very short or zero in which case we'll just replace the quote with '[...]')
+    """
     # Determine how much non-quote text is here
     def rec_textlength(node):
         text_length = 0
         for child in node.childGenerator():
             # Skip children that are blockquotes
-            if child.name and child.name == 'blockquote':
+            if child.name and child.name == "blockquote":
                 continue
             # Count length of string nodes
-            elif child.string:
+            if child.string:
                 if child.string != "\n":
                     text_length += len(child.string)
             # Skip children that are block spoilers
@@ -96,30 +102,32 @@ def _format_quotes(content, max_length, nesting):
     text_length = rec_textlength(content)
 
     # If we have any blockquotes, we want to format/truncate them recursively
-    blockquotes = content.find_all('blockquote', recursive=False)
+    blockquotes = content.find_all("blockquote", recursive=False)
 
     if blockquotes:
         # Find how much space can be devoted to each quote -- divide any remaining
         # space among them evenly
         remaining_length = (max_length - text_length) // len(blockquotes)
         for quote in blockquotes:
-            quote_by = quote.find('div', {'class': 'citey'})
-            quote_content = quote.find('div', {'class': 'quotey'})
+            quote_by = quote.find("div", {"class": "citey"})
+            quote_content = quote.find("div", {"class": "quotey"})
             if remaining_length > MIN_QUOTE_LENGTH:
                 # Recursively format inner quote text
                 # (base case is no quotes in which case this loop doesn't run)
                 _format_quotes(quote_content, remaining_length, nesting + 1)
-                markdown_quote = "\n".join(("> " + line) for line in quote_content.get_text().split("\n"))
+                markdown_quote = "\n".join(
+                    ("> " + line) for line in quote_content.get_text().split("\n")
+                )
             else:
-                markdown_quote = f'> {SNIP_TEXT}'
+                markdown_quote = f"> {SNIP_TEXT}"
 
             # if quote has a cite, add it
             if quote_by is not None:
-                markdown_quote = f'> **{quote_by.get_text()}**\n' + markdown_quote
+                markdown_quote = f"> **{quote_by.get_text()}**\n" + markdown_quote
 
             # If multiple quotes next to each other, add a separating line
             next_elem = quote.find_next_sibling()
-            if nesting > 0 or next_elem and next_elem.name and next_elem.name == 'blockquote':
+            if nesting > 0 or next_elem and next_elem.name and next_elem.name == "blockquote":
                 # we also add a separating line if nesting > 0 i.e. we're inside a nested quote,
                 # because discord doesn't actually support nested quotes so it can be hard to
                 # tell where a line ending with ">" ends and we aren't in the quote anymore
@@ -128,31 +136,27 @@ def _format_quotes(content, max_length, nesting):
             quote.replace_with(markdown_quote)
 
     def rec_truncate(node, deficit):
-        # Recursive function to truncate *non-quoted* text.
-        # 'deficit' is how much we have to cut off. But we
-        # want to avoid slicing quotes in half when truncating,
-        # so we want to remove them wholesale if we run into
-        # them rather than just cutting the result of get_text()
-        # at the very end. Hence the semi-convoluted logic here.
+        """
+        Recursive function to truncate *non-quoted* text.
 
-        # We iterate over the children of the node in reverse
-        # so we start cutting from the end
+        'deficit' is how much we have to cut off. But we want to avoid slicing quotes in half
+        when truncating, so we want to remove them wholesale if we run into them rather than
+        just cutting the result of get_text() at the very end. Hence the semi-convoluted logic.
+        """
+        # We iterate over the children of the node in reverse so we start cutting from the end
         for child in reversed(list(node.childGenerator())):
-            if child.name and child.name == 'blockquote':
-                # Since we still have a deficit (i.e. we're still
-                # looping, so we still want to be cutting stuff off
-                # from the end), just remove the quote wholesale since
-                # it's going to be after the '...' in the post
+            if child.name and child.name == "blockquote":
+                # Since we still have a deficit (i.e. we're still looping, so we still want to be
+                # cutting stuff off from the end), just remove the quote wholesale since it's going
+                # to be after the '...' in the post
                 child.clear()
             elif child.string:
                 if len(child.string) < deficit:
-                    # Remove the whole child and reduce the deficit
-                    # by its length.
+                    # Remove the whole child and reduce the deficit by its length.
                     deficit -= len(child.string)
                     child.string.replace_with("")
                 else:
-                    # We reduced the deficit enough! Should fit
-                    # under the character limit now.
+                    # We reduced the deficit enough! Should fit under the character limit now.
                     child.string.replace_with(child.string[:-deficit] + TRUNCATE_TEXT)
                     deficit = 0
                     break
@@ -162,80 +166,83 @@ def _format_quotes(content, max_length, nesting):
             else:
                 # It's some kind of weird compound tag
                 deficit = rec_truncate(child, deficit)
-                if deficit <= 0: break
+                if deficit <= 0:
+                    break
 
         return deficit
 
     if text_length > max_length:
         rec_truncate(content, text_length - max_length)
 
+
 def _convert_formatting(content):
     _format_quotes(content, FORUM_PREVIEW_LENGTH, 0)
 
     # Block spoilers replaced with title
-    block_spoilers = content.find_all('div', {'class': 'spoiler_container'})
+    block_spoilers = content.find_all("div", {"class": "spoiler_container"})
     for spoiler in block_spoilers:
-        if spoiler.find('button', {'class': 'spoileron'}):
+        if spoiler.find("button", {"class": "spoileron"}):
             # if it wasn't stripped out during the truncation process...
-            spoilertitle = spoiler.find('button', {'class': 'spoileron'}).get_text()
+            spoilertitle = spoiler.find("button", {"class": "spoileron"}).get_text()
             spoiler.replace_with(f"**[{spoilertitle}]**\n")
 
     # Spoilerize inline spoilers
-    inline_spoilers = content.find_all('span', {'class': 'inline_spoiler'})
+    inline_spoilers = content.find_all("span", {"class": "inline_spoiler"})
     for spoiler in inline_spoilers:
         # Drop the red spoiler title if there is one
         if spoiler.span:
             spoiler.span.clear()
-        new_spoiler = f'||{spoiler.get_text()}||'
+        new_spoiler = f"||{spoiler.get_text()}||"
         spoiler.replace_with(new_spoiler)
 
     # Basic formatting
-    bolds = content.find_all('strong')
+    bolds = content.find_all("strong")
     for bold in bolds:
         if bold.get_text():
-            bold.replace_with(f'**{bold.get_text()}**')
+            bold.replace_with(f"**{bold.get_text()}**")
 
-    italics = content.find_all('em')
+    italics = content.find_all("em")
     for italic in italics:
         if italic.get_text():
-            italic.replace_with(f'*{italic.get_text()}*')
+            italic.replace_with(f"*{italic.get_text()}*")
 
-    strikethroughs = content.find_all('del')
+    strikethroughs = content.find_all("del")
     for strikethrough in strikethroughs:
         if strikethrough.get_text():
-            strikethrough.replace_with(f'~~{strikethrough.get_text()}~~')
+            strikethrough.replace_with(f"~~{strikethrough.get_text()}~~")
 
-def _parse_forum_post(data):
-    '''
+
+def _parse_forum_post(data):  # pylint:disable=too-many-locals
+    """
     Pulls apart the AJAX data to find the bits of the forum post we want to display in
     the Discord embed.
-    '''
-    soup = BeautifulSoup(data[1], 'html.parser') # format is [postID, html]
+    """
+    soup = BeautifulSoup(data[1], "html.parser")  # format is [postID, html]
     post_id = data[0]
 
     # Header: sprite, username, badges
-    header = soup.find('div', {'class': 'post-header'})
+    header = soup.find("div", {"class": "post-header"})
     if header.h3.img:
-        user_sprite = header.h3.img['src']  # grabs the avatar if there is one (keep?)
+        user_sprite = header.h3.img["src"]  # grabs the avatar if there is one (keep?)
     else:
         user_sprite = None
     member = header.h3.a
-    user_profile = FORUM_ROOT + member['href']
+    user_profile = FORUM_ROOT + member["href"]
     if member.string:
         user_name = member.string
     else:
         user_name = _get_username(user_profile)
 
     # Footer: date, thumb score, utils (quote, report, permalink)
-    footer = soup.find('div', {'class': 'post-footer'})
-    post_date = footer.p.find('span', {'class': 'changeabletime'})['title']
-    utils = footer.find('ul', {'class': 'utils'})
-    permalink = utils.find('li', {'class': 'permalink'})
-    url = FORUM_ROOT + permalink.a['href']
+    footer = soup.find("div", {"class": "post-footer"})
+    post_date = footer.p.find("span", {"class": "changeabletime"})["title"]
+    utils = footer.find("ul", {"class": "utils"})
+    permalink = utils.find("li", {"class": "permalink"})
+    url = FORUM_ROOT + permalink.a["href"]
 
     # Body: message content, signature
-    body = soup.find('div', {'class': 'post-body'})
-    content = body.find('div', {'class': 'message-content'})
+    body = soup.find("div", {"class": "post-body"})
+    content = body.find("div", {"class": "message-content"})
 
     _convert_formatting(content)
 
@@ -244,84 +251,82 @@ def _parse_forum_post(data):
 
     # We may have just stripped away part of an inline spoiler, so fix that if needed
     # (should be an even number of '||' delimiters)
-    if text.count('||') % 2 != 0:
-        text += '||'
+    if text.count("||") % 2 != 0:
+        text += "||"
 
-    if text == '':
-        text = '_[post contains only images, quotes and/or spoilers]_'
+    if text == "":
+        text = "_[post contains only images, quotes and/or spoilers]_"
 
     post = {
-        'id': post_id,
-        'user_sprite': user_sprite,
-        'user_name': user_name,
-        'user_profile': user_profile,
-        'date': post_date,
-        'url': url,
-        'text': text,
+        "id": post_id,
+        "user_sprite": user_sprite,
+        "user_name": user_name,
+        "user_profile": user_profile,
+        "date": post_date,
+        "url": url,
+        "text": text,
     }
     return post
 
+
 def _post_in_discord(post):
-    '''
+    """
     Sends a webhook request to Discord, containing an embedded forum post.
     If an error occurs, will retry up to a total of 5 attempts before giving up.
-    '''
+    """
     # See: https://discord.com/developers/docs/resources/channel#embed-object
     embed_data = {
         # Alternate forum post colors
-        'color': (FORUM_COLOR_EVEN if int(post['id'][-1]) % 2 == 0 else FORUM_COLOR_ODD),
-        'author': {
-            'name': post['user_name'],
-            'url': post['user_profile']
-        },
-        'thumbnail': {
-            'url': post['user_sprite']
-        },
-        'description': f"{post['text']}\n\n{post['url']}"
+        "color": (FORUM_COLOR_EVEN if int(post["id"][-1]) % 2 == 0 else FORUM_COLOR_ODD),
+        "author": {"name": post["user_name"], "url": post["user_profile"]},
+        "thumbnail": {"url": post["user_sprite"]},
+        "description": f"{post['text']}\n\n{post['url']}",
     }
 
     print(f"Posting {post['id']} to Discord")
-    for attempt in range(5):
+    for _ in range(5):
         try:
             DISCORD_WEBHOOK.send(
                 embed=discord.Embed.from_dict(embed_data),
-                allowed_mentions=DISCORD_NO_MENTIONS
+                allowed_mentions=DISCORD_NO_MENTIONS,
             )
             return
         except discord.HTTPException as err:
-            print(f"{post['id']} attempt {attempt}, {err.status}: {err.text} (Discord code {err.code})")
+            print(f"{post['id']}, {err.status}: {err.text} (Discord code {err.code})")
             time.sleep(5)
     print(f"Failed to send {post['id']}")
 
+
 def is_board_excluded(url):
-    '''
-    Check whether a post belongs to a board we want to exclude from the forum spy, e.g. mafia boards
-    '''
+    """
+    Check whether a post belongs to a board we want to exclude from the forum spy
+    """
     for board in EXCLUDED_BOARDS:
         if board.lower() in url.lower():
             return True
     return False
 
 
-# # # # # 
+# # # # #
 # Functionality
 # # # # #
 
+
 def forum_spy_loop():
-    '''
+    """
     The main loop. Retrieves new forum posts every 15 seconds (same as forum spy).
     If there are any new posts, has them posted to the Discord channel.
-    '''
+    """
     newest_post_id = 0
     while True:
         try:
             # Request the forum spy data
             spy_request = urllib.request.Request(FORUM_SPY_AJAX, headers=FORUM_SPY_REQUEST_HEADERS)
-            with urllib.request.urlopen(spy_request) as f:
-                data = json.load(f)
+            with urllib.request.urlopen(spy_request) as response:
+                data = json.load(response)
         except urllib.error.HTTPError as err:
             # If an HTTP error occurs, wait 30s and try again
-            print(f'While querying forum spy, {err.code}: {err.reason}')
+            print(f"While querying forum spy, {err.code}: {err.reason}")
             time.sleep(30)
             continue
 
@@ -336,18 +341,17 @@ def forum_spy_loop():
                 if post_id > newest_post_id:
                     try:
                         post = _parse_forum_post(postdata)
-                    except Exception as e:
-                        print(f"While parsing {postdata[0]}, encountered {str(e)}")
+                    except Exception as err:  # pylint:disable=broad-except
+                        print(f"While parsing {postdata[0]}, encountered {str(err)}")
                         continue
-                    if not is_board_excluded(post['url']):
+                    if not is_board_excluded(post["url"]):
                         _post_in_discord(post)
                         time.sleep(1)
                     newest_post_id = post_id
-        
+
         # Wait a while before looking for new posts again
         time.sleep(15)
 
-if __name__ == '__main__':
-    # usually we'd probably kick off a worker thread doing this, but we only really do one thing,
-    # so we may as well just do it here.
+
+if __name__ == "__main__":
     forum_spy_loop()
--- test_spy.py
+++ test_spy.py
@@ -1,85 +1,56 @@
+"""
+Quick-and-dirty testing for the forum spy bot. Allows testing with specific forum posts to
+find and address odd edge cases.
+"""
+# pylint:disable=protected-access
 import argparse
-from bs4 import BeautifulSoup
 import json
 import os
 import time
 import urllib.request
 
+from bs4 import BeautifulSoup
+
 import forum_spy
 
 
 # for now, not committed to the repo; generated and used locally only
-TEST_DATA_FILE = 'test_data.ajax'
+TEST_DATA_FILE = "test_data.ajax"
 
 
 def make_forum_post_ajax(post_id):
-    '''
+    """
     Queries for a post with a given ID number (int) and generates the AJAX data for it,
     as if it were returned from the forum spy AJAX request.
-    '''
-    message_url = f'{forum_spy.FORUM_ROOT}/forum/message/{post_id}'
+    """
+    message_url = f"{forum_spy.FORUM_ROOT}/forum/message/{post_id}"
     try:
         post_request = urllib.request.Request(
             message_url, headers=forum_spy.FORUM_SPY_REQUEST_HEADERS
         )
-        with urllib.request.urlopen(post_request) as f:
-            data = f.read()
+        with urllib.request.urlopen(post_request) as response:
+            data = response.read()
     except urllib.error.HTTPError as err:
-        print(f'While querying {message_url}, {err.code}: {err.reason}')
+        print(f"While querying {message_url}, {err.code}: {err.reason}")
         return None
 
-    soup = BeautifulSoup(data, 'html.parser')
-    id_str = f'post{post_id}'
-    post_html = str(soup.find('div', {'id': id_str}))
-    
+    soup = BeautifulSoup(data, "html.parser")
+    id_str = f"post{post_id}"
+    post_html = str(soup.find("div", {"id": id_str}))
+
     return [id_str, post_html]
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description='Test the forum spy AJAX parser, or generate test data'
-    )
-    parser.add_argument(
-        '-a', '--add-post',
-        type = int,
-        nargs = '+',
-        required = False,
-        help = 'One or more post IDs (int) to add to the test AJAX data'
-    )
-    parser.add_argument(
-        '-d', '--delete-post',
-        type = int,
-        nargs = '+',
-        required = False,
-        help = 'One or more post IDs (int) to remove from the test AJAX data'
-    )
-    parser.add_argument(
-        '-t', '--test',
-        action = 'store_true',
-        help = 'Test the parser against the data in the test set'
-    )
-    parser.add_argument(
-        '-p', '--post',
-        action = 'store_true',
-        help = 'Post the embed data to Discord (WARNING: ensure you are using a test webhook!)'
-    )
-    parser.add_argument(
-        '--clear',
-        action = 'store_true',
-        help = 'Remove all of the test data from the AJAX file (can use --add-post to replace data)'
-    )
-    args = parser.parse_args()
-
-    # arg conflicts
-    if args.post and not args.test:
-        print('Warning: --test not specified; --post will be ignored!')
-
+def main(args):
+    """
+    Run the parser agaist the test set
+    """
     # load the test data
     test_data = []
     if os.path.isfile(TEST_DATA_FILE):
-        with open(TEST_DATA_FILE, 'r') as f:
-            test_data = json.load(f)
-    
+        with open(TEST_DATA_FILE, "r") as ajax:
+            test_data = json.load(ajax)
+
     # add/remove any specified test cases
     if args.add_post or args.delete_post or args.clear:
         if args.clear:
@@ -89,20 +60,57 @@ if __name__ == '__main__':
                 test_data.append(make_forum_post_ajax(post_id))
                 time.sleep(0.5)
         if args.delete_post:
-            test_data = [
-                p for p in test_data if int(p[0][4:]) not in args.delete_post
-            ]
+            test_data = [p for p in test_data if int(p[0][4:]) not in args.delete_post]
         # save the data back to the file
-        with open(TEST_DATA_FILE, 'w') as f:
-            json.dump(test_data, f)
-    
+        with open(TEST_DATA_FILE, "w") as ajax:
+            json.dump(test_data, ajax)
+
     # test the parser against the test data
-    if args.test:
+    if args.test or args.post:
         for post_data in test_data:
             post = forum_spy._parse_forum_post(post_data)
             # post to discord if instructed
             if args.post:
                 forum_spy._post_in_discord(post)
                 time.sleep(1)
-    
-    print('done')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Test the forum spy AJAX parser, or generate test data"
+    )
+    parser.add_argument(
+        "-a",
+        "--add-post",
+        type=int,
+        nargs="+",
+        required=False,
+        help="One or more post IDs (int) to add to the test AJAX data",
+    )
+    parser.add_argument(
+        "-d",
+        "--delete-post",
+        type=int,
+        nargs="+",
+        required=False,
+        help="One or more post IDs (int) to remove from the test AJAX data",
+    )
+    parser.add_argument(
+        "-t",
+        "--test",
+        action="store_true",
+        help="Test the parser against the data in the test set",
+    )
+    parser.add_argument(
+        "-p",
+        "--post",
+        action="store_true",
+        help="Post the embed data to Discord (WARNING: ensure you are using a test webhook!)",
+    )
+    parser.add_argument(
+        "--clear",
+        action="store_true",
+        help="Remove all of the test data from the AJAX file (can use --add-post to replace data)",
+    )
+    main(parser.parse_args())
+    print("done")
